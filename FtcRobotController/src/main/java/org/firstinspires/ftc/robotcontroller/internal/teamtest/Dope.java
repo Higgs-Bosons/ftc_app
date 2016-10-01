d623e856a10da97f72fe57c43c66d9f9a8b9d17e
Higgs Bosons <ojasa2012@gmail.com>
2016-10-01 10:18:05 -0400

    Testing From O

---
+++ FtcRobotController/src/main/java/org/firstinspires/ftc/robotcontroller/internal/teamtest/Dope.java
@@ -0,0 +1,36 @@
+package org.firstinspires.ftc.robotcontroller.internal.teamtest;
+
+/**
+ * Created by Higgs Bosons on 9/28/2016.
+ */
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name="Doper", group ="Tests")
+public class Dope extends LinearOpMode {
+    DcMotor leftFront;
+    DcMotor leftBack;
+    DcMotor rightFront;
+    DcMotor rightBack;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        leftBack = hardwareMap.dcMotor.get("leftBack");
+        rightBack = hardwareMap.dcMotor.get("rightBack");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftFront.setDirection(DcMotor.Direction.REVERSE);
+        leftBack.setDirection(DcMotor.Direction.REVERSE);
+
+        waitForStart();
+
+        leftFront.setPower(0.5d);
+        leftBack.setPower(0.5d);
+        rightBack.setPower(0.5d);
+        rightFront.setPower(0.5d);
+
+        Thread.sleep(1000);
+    }
+}
