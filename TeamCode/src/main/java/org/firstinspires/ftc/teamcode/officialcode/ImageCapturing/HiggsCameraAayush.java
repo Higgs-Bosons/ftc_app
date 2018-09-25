0b33cca0a4df50c73b76ba7dd336179d7ccc7cc1
Sonic49 <ftc7136@gmail.com>
2018-09-24 20:13:15 -0400

    hi

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/HiggsCameraAayush.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/HiggsCameraAayush.java
@@ -1,18 +1,20 @@
 package org.firstinspires.ftc.teamcode.officialcode.ImageCapturing;
 
-import android.graphics.Camera;
-import android.graphics.Canvas;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+
 @Autonomous(name = "Camera Tester Aayush", group = "Tester")
 public class HiggsCameraAayush extends LinearOpMode{
-    public void runOpMode() throws InterruptedException {
-        Camera camera = new Camera();
-        Canvas canvas = new Canvas();
 
-        camera.applyToCanvas(canvas);
+    public void runOpMode() throws InterruptedException {
+        waitForStart();
+
+
+
+        while (opModeIsActive()) {
+
+        }
 
     }
 
