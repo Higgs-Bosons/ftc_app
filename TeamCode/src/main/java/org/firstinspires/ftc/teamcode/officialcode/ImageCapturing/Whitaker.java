91532fcf042ca83428cfa33918e6dd0cb4c54a46
wjkohls2002 <wjkohls2002@gmail.com>
2018-09-27 18:46:30 -0400

    09/27/18 18:46:30

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/Whitaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/Whitaker.java
@@ -6,7 +6,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 @Autonomous(name = "Camera-Whitaker", group = "Tester")
 public class Whitaker extends LinearOpMode {
-
     public void runOpMode() throws InterruptedException {
     }
 
