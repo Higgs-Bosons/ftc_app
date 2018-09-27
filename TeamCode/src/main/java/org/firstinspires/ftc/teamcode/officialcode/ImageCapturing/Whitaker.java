968dbc31fced212dfe970c0a0c612e4f042251b1
wjkohls2002 <wjkohls2002@gmail.com>
2018-09-27 18:42:13 -0400

    09/27/18 18:42:13

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/Whitaker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/Whitaker.java
@@ -1,18 +1,7 @@
 package org.firstinspires.ftc.teamcode.officialcode.ImageCapturing;
 
-import android.content.Context;
-import android.hardware.Camera;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.widget.FrameLayout;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-
-import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
-import org.firstinspires.ftc.robotcore.internal.system.AppUtil;
-
-import java.io.IOException;
 
 
 @Autonomous(name = "Camera-Whitaker", group = "Tester")
