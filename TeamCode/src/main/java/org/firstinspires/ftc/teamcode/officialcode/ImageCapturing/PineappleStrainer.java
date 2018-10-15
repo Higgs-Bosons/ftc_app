f5b43e2c534df0beec8a45d1e7451d17c3173d3a
wjkohls2002 <wjkohls2002@gmail.com>
2018-10-15 19:39:47 -0400

    10/15/18 07:39:47 PM (WJK)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/PineappleStrainer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/officialcode/ImageCapturing/PineappleStrainer.java
@@ -43,7 +43,7 @@ public class PineappleStrainer {
     }
 
     public void findYellowCube(Bitmap picture, int precision){
-        int PRECISION = (picture.getHeight()*(100/precision));
+        int PRECISION = ((int) ((picture.getHeight()*((100.0-precision)/100.0))+1));
         int MostYellow = 0;
         int PixelColor;
         int currentPixelYellow;
@@ -97,10 +97,39 @@ public class PineappleStrainer {
             OneLine = new StringBuilder("|");
         }
 
+        for(int Y = 0; Y < cords[0].length; Y ++) {
+            for(int X = 0; X < cords.length; X++){
+                if(cords[X][Y]){
+                    int size = 0;
+                    int LeftX  = X;
+                    int RightX = X;
+                    int UpY    = Y;
+                    int DownY  = Y;
+                    for(int counterX = X; counterX >= 0; counterX--){
+                        if(cords[counterX][Y]){
+                            size++;
+                            LeftX = (counterX > LeftX) ? counterX : LeftX;
+                        }else{
+                            counterX = -1;
+                        }
+                    }
+                    for(int counterX = X; counterX <= cords.length; counterX++){
+                        if(cords[counterX][Y]){
+                            size++;
+                            RightX = (counterX > RightX) ? counterX : RightX;
+                        }else{
+                            counterX = cords.length+10;
+                        }
+                    }
+                    Log.d("Size", size+"");
+                    Log.d("LeftX", LeftX+"");
+                    Log.d("RightX", RightX+"");
+                }
+            }
+        }
 
 
         Log.d("Time", (finish - start)+" mls");
 
     }
-
 }
