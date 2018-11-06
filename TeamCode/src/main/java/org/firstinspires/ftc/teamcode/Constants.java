cbd694123596c589ac9fa55459b932a4ca905183
wjkohls2002 <wjkohls2002@gmail.com>
2018-11-05 19:37:05 -0500

    11/05/18 07:37:05 PM (WJK)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -13,14 +13,17 @@ public class Constants {
     public static final String FIRST_LETTER_WITH_SPACE_UPPERCASE = "Example: L F";
     public static final String FIRST_LETTER_WITH_UNDERSCORE_UPPERCASE = "Example: L_F";
 
-    
+    public static final String FIRST_LETTER_NO_SPACE_LOWERCASE = "Example: lf";
+    public static final String FIRST_LETTER_WITH_SPACE_LOWERCASE = "Example: l f";
+    public static final String FIRST_LETTER_WITH_UNDERSCORE_LOWERCASE = "Example: l_f";
 
     public static final String FULL_NAME_NO_SPACE = "Example: LeftFront";
     public static final String FULL_NAME_WITH_SPACE = "Example: Left Front";
     public static final String FULL_NAME_WITH_UNDERSCORE = "Example: Left_Front";
 
-    public static final String FULL_NAME_NO_SPACE_ALL_CAPS = "Example: LEFTFRONT";
-    public static final String FULL_NAME_WITH_SPACE_ALL_CAPS = "Example: LEFT FRONT";
-    public static final String FULL_NAME_WITH_UNDERSCORE_ALL_CAPS = "Example: LEFT_FRONT";
+    public static final String FULL_NAME_NO_SPACE_UPPERCASE = "Example: LEFTFRONT";
+    public static final String FULL_NAME_WITH_SPACE_UPPERCASE = "Example: LEFT FRONT";
+    public static final String FULL_NAME_WITH_UNDERSCORE_UPPERCASE = "Example: LEFT_FRONT";
+
 
 }
