395fa90000d9bf9b47278b20d224079b7e64df1b
wjkohls2002 <wjkohls2002@gmail.com>
2018-12-01 14:37:21 -0500

    12/01/18 02:37:20 PM (WJK)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Galaxy/Autonomus/Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Galaxy/Autonomus/Test.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode.Galaxy.Autonomus;
 
 public class Test {
+
+
+    class Autonomus implements Runnable {
+
+        public void run() {
+            
+        }
+    }
 }
