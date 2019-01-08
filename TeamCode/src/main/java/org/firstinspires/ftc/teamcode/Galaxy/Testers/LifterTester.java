881a3855ed2574ac0173ea59a0245b4fc61dc21d
Sonic49 <ftc7136@gmail.com>
2019-01-08 18:42:36 -0500

    01/08/19 06:42:36 PM (AV)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Galaxy/Testers/LifterTester.java
@@ -0,0 +1,30 @@
+package org.firstinspires.ftc.teamcode.Galaxy.Testers;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+@Autonomous(name = "LifterTester", group = "Tester")
+public class LifterTester extends LinearOpMode {
+
+
+    public void runOpMode() {
+        DcMotor PowerUp, PowerDown;
+        PowerUp = hardwareMap.dcMotor.get("PowerUp");
+        PowerDown = hardwareMap.dcMotor.get("PowerDown");
+        waitForStart();
+        while (opModeIsActive()){
+            if(gamepad1.dpad_up) {
+                PowerUp.setPower(0.8);
+                PowerDown.setPower(0.8);
+            }
+            else if (gamepad1.dpad_down) {
+                PowerUp.setPower(-0.8);
+                PowerDown.setPower(-0.8);
+            }else{
+                PowerUp.setPower(0);
+                PowerDown.setPower(0);
+            }
+        }      
+    }
+}
